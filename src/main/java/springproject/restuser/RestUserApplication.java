dedbb6f89816cf35ed0ec0ad5f4f0b87674a5ebb
Nicolas <aponcenicolas@gmail.com>
2020-11-09 18:31:24 -0500

    add user crud with pagination

---
+++ src/main/java/springproject/restuser/RestUserApplication.java
@@ -0,0 +1,13 @@
+package springproject.restuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springproject/restuser/RestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject.restuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
